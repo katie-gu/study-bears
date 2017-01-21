00d5e09d88344692fc7fb109104d9910fea4ae90
Jhinuk Barman <jhinukbarman@Jhinuks-MacBook.local>
2017-01-20 23:28:20 -0800

    corrected if statement syntax

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -4,7 +4,7 @@
 public class LeapYear {
 
     public static boolean isLeapYear(int year){
-        if (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
             return true;
         }
         else {
