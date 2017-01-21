05e14a01aa19778e5323f8e8b3827ee632469f9e
Jhinuk Barman <jhinukbarman@Jhinuks-MacBook.local>
2017-01-20 23:24:44 -0800

    changed leap year if statement

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -4,11 +4,11 @@
 public class LeapYear {
 
     public static boolean isLeapYear(int year){
-        if (year % 4 != 0) {
-            return false;
+        if (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) {
+            return true;
         }
         else {
-            return true;
+            return false;
         }
     }
     /** Calls isLeapYear to print correct statement.
