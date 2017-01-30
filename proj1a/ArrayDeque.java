5fc21fc60de8b59ca7be68c680773cff890c69b8
Jhinuk Barman <jhinukbarman@airbears2-10-142-86-73.airbears2.1918.berkeley.edu>
2017-01-29 22:36:12 -0800

    added empty methods to ArrayDeque.java

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,43 @@
+/**
+ * Created by jhinukbarman on 1/29/17.
+ */
+public class ArrayDeque<Item> {
+
+    public ArrayDeque(){
+        
+    }
+
+    public void addFirst(Item){
+
+    }
+
+    public void addLast(Item){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public int size(){
+
+    }
+
+    public void printDeque(){
+
+    }
+
+    public Item removeFirst(){
+
+    }
+
+    public Item removeLast(){
+
+    }
+
+    public Item get(int index){
+
+    }
+
+
+}
