38f2a757d0fd49681e5f8a39e5fbe1e3b8e3329d
Jhinuk Barman <jhinukbarman@calvisitor-10-105-183-137.calvisitor.1918.berkeley.edu>
2017-01-30 15:50:54 -0800

    added instance variables to ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,16 +3,33 @@
  */
 public class ArrayDeque<Item> {
 
+    private Item[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private int usageRatio;
+
     public ArrayDeque(){
-        
+        items = (Item[]) new Object[8];
+        size = 0;
     }
 
-    public void addFirst(Item){
-
+    public void addFirst(Item i){
+        //sentinel.first = i;
+        size++;
     }
 
-    public void addLast(Item){
-
+    private void resize(int capacity){
+        Item[] a = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
+    }
+    public void addLast(Item i){
+        if (size == items.length){
+            resize(size + 1);
+        }
+        items[size] = i;
+        size++;
     }
 
     public boolean isEmpty(){
@@ -20,7 +37,7 @@ public class ArrayDeque<Item> {
     }
 
     public int size(){
-
+        return size;
     }
 
     public void printDeque(){
