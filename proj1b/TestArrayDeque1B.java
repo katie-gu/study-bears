d2f7e8f5a2603bf5731a0e8f50a033f811cb6cbd
Jhinuk Barman <jhinukbarman@airbears2-10-142-84-173.airbears2.1918.berkeley.edu>
2017-02-07 21:39:35 -0800

    completed TestArrayDeque1B.java

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -11,38 +11,48 @@ public class TestArrayDeque1B {
      * This code was borrowed from StudentArrayDequeLauncher.
      */
     @Test
-    public static void randomTest(){
+    public void randomTest() {
+        OperationSequence fs = new OperationSequence();
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
 
-        for (int i = 0; i < 10; i += 1) {
+        for (int i = 0; i < 1000; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
 
             if (numberBetweenZeroAndOne < 0.5) {
+                DequeOperation dequeOp1 = new DequeOperation("addFirst", i);
+                fs.addOperation(dequeOp1);
                 sad1.addLast(i);
                 ads1.addLast(i);
-
             } else {
+                if ((!sad1.isEmpty()) && (!ads1.isEmpty())) {
+                    DequeOperation dequeOp3 = new DequeOperation("removeFirst");
+                    fs.addOperation(dequeOp3);
+                    Integer actual = sad1.removeFirst();
+                    Integer expected = ads1.removeFirst();
+                    assertEquals(fs.toString(), expected, actual);
+                }
+
+                if ((!sad1.isEmpty()) && (!ads1.isEmpty())) {
+                    DequeOperation dequeOp4 = new DequeOperation("removeLast");
+                    fs.addOperation(dequeOp4);
+                    Integer actual = sad1.removeLast();
+                    Integer expected = ads1.removeLast();
+                    assertEquals(fs.toString(), expected, actual);
+                }
+
+                DequeOperation dequeOp2 = new DequeOperation("addLast", i);
+                fs.addOperation(dequeOp2);
                 sad1.addFirst(i);
                 ads1.addFirst(i);
+
             }
         }
-        System.out.println("hi");
-
-        if ((!sad1.isEmpty()) && (!ads1.isEmpty())) {
-            Integer x = sad1.removeFirst();
-            Integer y = ads1.removeFirst();
-            //assertEquals(x, y);
-
-        }
-
-        sad1.printDeque();
-        ads1.printDeque();
     }
 
     public static void main(String[] args) {
-        System.out.println("hellloo");
-       // randomTest();
+        TestArrayDeque1B test1 = new TestArrayDeque1B();
+        test1.randomTest();
         /* Helpful challenge: Modify this file so that it outputs the list of
            operations as a String. Use the OperationSequence class. */
     }
