8928257f88439942036a5baee00ae2284dde31e3
Jhinuk Barman <jhinukbarman@airbears2-10-142-84-173.airbears2.1918.berkeley.edu>
2017-02-08 14:55:26 -0800

    OffByOne.java edited

---
+++ proj1b/OffByOne.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jhinukbarman on 2/8/17.
+ */
+public class OffByOne implements CharacterComparator{
+    //static?
+    @Override
+    public boolean equalChars(char x, char y){
+        
+    }
+
+}
