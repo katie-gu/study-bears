5c452edfade9944cc59dce2e0197c3b48be35b61
Jhinuk Barman <jhinukbarman@airbears2-10-142-84-173.airbears2.1918.berkeley.edu>
2017-02-08 14:01:44 -0800

    edited Palindrome.java

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -8,12 +8,30 @@ public class Palindrome{
         for (int i = word.length() - 1; i >= 0; i--){
             chars.addFirst(word.charAt(i));
         }
-        chars.printDeque();
+        //chars.printDeque();
         return chars;
     }
     public static boolean isPalindrome(String word){
-        return true;
+        Deque<Character> chars = wordToDeque(word);
+        if (word.length() == 0 || word.length() == 1) {
+            return true;
+        }
 
+        int first = 0;
+        int last = word.length()-1;
+        char compare1 = chars.get(first);
+        char compare2 = chars.get(last);
+
+        while(compare1 == compare2){
+            first++;
+            last--;
+            compare1 = chars.get(first);
+            compare2 = chars.get(last);
+            if(first == last || first>last){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
