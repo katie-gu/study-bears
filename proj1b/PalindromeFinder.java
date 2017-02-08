aa567a3588b154924a8eab01f9e2c6c991987440
Jhinuk Barman <jhinukbarman@airbears2-10-142-84-173.airbears2.1918.berkeley.edu>
2017-02-08 14:02:52 -0800

    edited PalindromeFinder.java

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,6 +1,8 @@
 /** This class outputs all palindromes in the words file in the current directory. */
 public class PalindromeFinder {
     public static void main(String[] args) {
+        Palindrome.wordToDeque("hello");
+        //Palindrome.isPalindrome("hello");
         /*int minLength = 4;
         In in = new In("words");
 
