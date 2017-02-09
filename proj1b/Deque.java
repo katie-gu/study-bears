888929fb1b5dfd085dcf50acce96c7dd5d2a4e4b
Jhinuk Barman <jhinukbarman@calvisitor-10-105-139-139.calvisitor.1918.berkeley.edu>
2017-02-09 15:04:26 -0800

    fixed style Deque.java

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -3,13 +3,13 @@
  * Created by jhinukbarman on 2/7/17.
  */
 public interface Deque<Item> {
-    public void addFirst(Item x);
-    public void addLast(Item x);
-    public boolean isEmpty();
-    public int size();
-    public void printDeque();
-    public Item removeFirst();
-    public Item removeLast();
-    public Item get(int index);
+    void addFirst(Item x);
+    void addLast(Item x);
+    boolean isEmpty();
+    int size();
+    void printDeque();
+    Item removeFirst();
+    Item removeLast();
+    Item get(int index);
 
 }
