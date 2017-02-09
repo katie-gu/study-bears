18e4ffeb3b6bf3a636516856826c160f3b58e63b
Jhinuk Barman <jhinukbarman@calvisitor-10-105-139-139.calvisitor.1918.berkeley.edu>
2017-02-09 15:01:26 -0800

    completed ArrayDequeSolution.java

--- proj1b/ArrayDequeSolution.java
+++ proj1b/ArrayDequeSolution.java
@@ -1,22 +1,15 @@
 import java.util.LinkedList;
-
 /**
  * Isn't this solution kinda... cheating? Yes.
  * The aesthete will be especially alarmed by the fact that this
  * supposed ArrayDeque is actually using a LinkedList. SAD!
  */
-<<<<<<< HEAD
-public class ArrayDequeSolution<Item> extends LinkedList<Item> implements Deque<Item>{
+public class ArrayDequeSolution<Item> extends LinkedList<Item> implements Deque<Item> {
     public void printDeque() {
         for (int i = 0; i < size(); i++) {
             System.out.print(get(i) + " ");
         }
         //System.out.println("dummy");
-=======
-public class ArrayDequeSolution<Item> extends LinkedList<Item> {
-    public void printDeque() {
-        System.out.println("dummy");
->>>>>>> 28d2613fc341c86308ee47cb0702dae1ce0d65a5
     }
 
     public Item getRecursive(int i) {
