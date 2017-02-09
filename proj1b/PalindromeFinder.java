36753be80f1f6e7abe0a541846f80553a7d5b717
Jhinuk Barman <jhinukbarman@calvisitor-10-105-139-139.calvisitor.1918.berkeley.edu>
2017-02-09 14:59:26 -0800

    completed PalindromeFinder.java

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,24 +1,17 @@
 /** This class outputs all palindromes in the words file in the current directory. */
 public class PalindromeFinder {
     public static void main(String[] args) {
-<<<<<<< HEAD
         int minLength = 4;
         In in = new In("words.txt");
-=======
-        /*int minLength = 4;
-        In in = new In("words");
->>>>>>> 28d2613fc341c86308ee47cb0702dae1ce0d65a5
+        OffByOne obo = new OffByOne();
+        OffByN obn = new OffByN(5);
 
         while (!in.isEmpty()) {
             String word = in.readString();
-            if (word.length() >= minLength && Palindrome.isPalindrome(word)) {
+            if (word.length() >= minLength && Palindrome.isPalindrome(word, obn)) {
                 System.out.println(word);
             }
-<<<<<<< HEAD
         }
-        System.out.println('a'-'c');
-=======
-        }*/
->>>>>>> 28d2613fc341c86308ee47cb0702dae1ce0d65a5
+
     }
 } 
