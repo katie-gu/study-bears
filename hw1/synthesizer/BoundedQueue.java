111bc71c7d01585bb1094107ab29f069729036a9
Jhinuk Barman <jhinukbarman@Jhinuks-MacBook.local>
2017-02-19 21:27:31 -0800

    finished BoundedQueue.java

--- hw1/synthesizer/BoundedQueue.java
+++ hw1/synthesizer/BoundedQueue.java
@@ -1,9 +1,9 @@
 package synthesizer;
-
+import java.util.Iterator;
 /**
  * Created by jhinukbarman on 2/18/17.
  */
-public interface BoundedQueue<T> {
+public interface BoundedQueue<T> extends Iterable<T>{
     int capacity();          // return size of the buffer
     int fillCount();         // return number of items currently in the buffer
     void enqueue(T x);  // add item x to the end
@@ -19,4 +19,6 @@ public interface BoundedQueue<T> {
     default boolean isFull(){
         return fillCount() == capacity();
     }
+
+    Iterator<T> iterator();
 }
