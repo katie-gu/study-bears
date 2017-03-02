ecef69eec7fdc24a0b6d682b672e437bb0b32d81
Jhinuk Barman <jhinukbarman@airbears2-10-142-85-8.airbears2.1918.berkeley.edu>
2017-03-01 16:55:48 -0800

    update

--- proj2/db/Database.java
+++ proj2/db/Database.java
@@ -1,11 +1,43 @@
 package db;
 
+import java.util.HashMap;
+
 public class Database {
-    public Database() {
+    public static HashMap<String, Table> h;
+    Table myTable;
+
+    public Database(){
+        h = new HashMap<String, Table>();
+    }
+/*
+    public Database(Table t) {
         // YOUR CODE HERE
+        h = new HashMap<String, Table>();
+        myTable = t;
+        h.put(myTable.getTableName(), myTable);
+       // ArrayList<>;
+    }
+ */
+
+    public void join(HashMap<String, Column> a, HashMap<String, Column> b){
+        for (String key : a.keySet()){
+
+        }
     }
 
     public String transact(String query) {
-        return "YOUR CODE HERE";
+       // Parse.
+        Parser p = new Parser(this);
+        p.eval(query);
+        return "from dataBase class hi";
+    }
+
+    public HashMap<String, Table> getMap(){
+        return h;
+    }
+
+
+    public void addTable(String tableName, Table table) {
+        h.put(tableName, table);
     }
 }
