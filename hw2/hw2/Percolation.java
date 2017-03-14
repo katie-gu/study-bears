30595e7b384d806a438dcf6248afb4fddba5866c
Jhinuk Barman <jhinukbarman@Jhinuks-MacBook.local>
2017-03-14 15:26:21 -0700

    update

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,13 +4,12 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private boolean[][] arr;
-    WeightedQuickUnionUF weightedU;
-    WeightedQuickUnionUF percolateTracker;
+    private WeightedQuickUnionUF weightedU; //keep track of what is full and what isnt
+    private WeightedQuickUnionUF percolateTracker;
     private int topUnion;
     private int bottomUnion;
-    private boolean prev; //do i need this?
-    private int curr; //do i need this?
     private int count;
+
 
     public Percolation(int N) { // create N-by-N grid, with all sites initially blocked
         if (N <= 0) {
@@ -18,6 +17,7 @@ public class Percolation {
         }
         arr = new boolean[N][N];
         weightedU = new WeightedQuickUnionUF(10000);
+        percolateTracker = new WeightedQuickUnionUF(10000);
         topUnion = 9999;
         bottomUnion = 8888;
         count = 0;
@@ -31,20 +31,28 @@ public class Percolation {
             int num = rcto1D(row, col);
             if (row == 0) {
                 weightedU.union(num, topUnion);
+                percolateTracker.union(num, topUnion);
             }
-            if (row == (arr.length - 1)) {
-                if (isFull(row, col)) {
-                    weightedU.union(num, bottomUnion);
-                } else {
-                    percolateTracker.union(num, bottomUnion);
-                }
-            }
+
             arr[row][col] = true;
             checkNeighbors(row, col, num);
+            if (row == (arr.length - 1)) {
+                //if (isFull(row, col)) {
+                    weightedU.union(num, bottomUnion);
+                // else {
+                    //percolateTracker.union(num, bottomUnion);
+                    //bottomCheck = num;
+                //}
+            }
+            //if (weightedU.connected(num, bottomCheck) && isFull(row, col)) {
+               // weightedU.union(num, bottomUnion);
+            //}
             //prev = arr[row][col];
+
             count += 1;
         }
     }
+
 
     private void checkNeighbors(int row, int col, int numConvert) {
         int newNum;
@@ -53,99 +61,197 @@ public class Percolation {
             if ((isOpen(row, col + 1))) {
                 newNum = rcto1D(row, col + 1);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+               // if (isFull(row, col)) {
+               //     percolateTracker.union(numConvert, newNum);
+              //  }
             }
             if ((row != 0) && (row != arr.length - 1)) {
                 if (isOpen(row - 1, col)) {
                     newNum = rcto1D(row - 1, col);
                     weightedU.union(numConvert, newNum);
+                    percolateTracker.union(numConvert, newNum);
+
+                    // if (isFull(row, col)) {
+                   //     percolateTracker.union(numConvert, newNum);
+                  //  }
                 }
                 if (isOpen(row + 1, col)) {
                     newNum = rcto1D(row + 1,col);
                     weightedU.union(numConvert, newNum);
+                    percolateTracker.union(numConvert, newNum);
+
+                    //   if (isFull(row, col)) {
+                  //      percolateTracker.union(numConvert, newNum);
+                  //  }
                 }
 
             } else if (row == 0) {
                 if (isOpen(row + 1, col)) {
                     newNum = rcto1D(row + 1, col);
                     weightedU.union(numConvert, newNum);
+                    percolateTracker.union(numConvert, newNum);
+
+                    //   if (isFull(row, col)) {
+                 //       percolateTracker.union(numConvert, newNum);
+                 //   }
                 }
             } else { // row = arr.length - 1
                 if (isOpen(row - 1, col)) {
                     newNum = rcto1D(row - 1, col);
                     weightedU.union(numConvert, newNum);
+                    percolateTracker.union(numConvert, newNum);
+
+                    //     if (isFull(row, col)) {
+                //        percolateTracker.union(numConvert, newNum);
+                //    }
                 }
             }
         } else if (col == arr[0].length - 1) {
             if ((isOpen(row, col - 1))) {
                 newNum = rcto1D(row, col - 1);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                // if (isFull(row, col)) {
+               //     percolateTracker.union(numConvert, newNum);
+              //  }
             }
             if ((row != 0) && (row != (arr.length - 1))) {
                 if (isOpen(row - 1, col)) {
                     newNum = rcto1D(row - 1, col);
                     weightedU.union(numConvert, newNum);
+                    percolateTracker.union(numConvert, newNum);
+
+                    //  if (isFull(row, col)) {
+                  //      percolateTracker.union(numConvert, newNum);
+                  //  }
                 }
                 if (isOpen(row + 1, col)) {
                     newNum = rcto1D(row + 1,col);
                     weightedU.union(numConvert, newNum);
+                    percolateTracker.union(numConvert, newNum);
+
+                    //  if (isFull(row, col)) {
+                  //      percolateTracker.union(numConvert, newNum);
+                 //   }
                 }
 
             } else if (row == 0) {
                 if (isOpen(row + 1, col)) {
                     newNum = rcto1D(row + 1, col);
                     weightedU.union(numConvert, newNum);
+                    percolateTracker.union(numConvert, newNum);
+
+                    //  if (isFull(row, col)) {
+                 //       percolateTracker.union(numConvert, newNum);
+                  //  }
                 }
             } else { // row = arr.length - 1
                 if (isOpen(row - 1, col)) {
                     newNum = rcto1D(row - 1, col);
                     weightedU.union(numConvert, newNum);
+                    percolateTracker.union(numConvert, newNum);
+
+                    //   if (isFull(row, col)) {
+                 //       percolateTracker.union(numConvert, newNum);
+                 //   }
                 }
             }
         } else if (row == 0) {
             if (isOpen(row, col + 1)) {
                 newNum = rcto1D(row, col + 1);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                //  if (isFull(row, col)) {
+              //      percolateTracker.union(numConvert, newNum);
+              //  }
             }
             if (isOpen(row, col - 1)) {
                 newNum = rcto1D(row, col - 1);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                //  if (isFull(row, col)) {
+              //      percolateTracker.union(numConvert, newNum);
+               // }
             }
             if (isOpen(row + 1, col)) {
                 newNum = rcto1D(row + 1, col);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                //  if (isFull(row, col)) {
+              //      percolateTracker.union(numConvert, newNum);
+              //  }
             }
         } else if (row == arr.length - 1) {
             if (isOpen(row, col + 1)) {
                 newNum = rcto1D(row, col + 1);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                //  if (isFull(row, col)) {
+               //     percolateTracker.union(numConvert, newNum);
+               // }
             }
             if (isOpen(row, col - 1)) {
                 newNum = rcto1D(row, col - 1);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                //  if (isFull(row, col)) {
+              //      percolateTracker.union(numConvert, newNum);
+              //  }
             }
             if (isOpen(row - 1, col)) {
                 newNum = rcto1D(row - 1, col);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                // if (isFull(row, col)) {
+              //      percolateTracker.union(numConvert, newNum);
+               // }
             }
         } else {
             if (isOpen(row, col + 1)) {
                 newNum = rcto1D(row, col + 1);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                // if (isFull(row, col)) {
+               //     percolateTracker.union(numConvert, newNum);
+               // }
             }
             if (isOpen(row, col - 1)) {
                 newNum = rcto1D(row, col - 1);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                //  if (isFull(row, col)) {
+               //     percolateTracker.union(numConvert, newNum);
+              //  }
             }
             if (isOpen(row - 1, col)) {
                 newNum = rcto1D(row - 1, col);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                //  if (isFull(row, col)) {
+              //      percolateTracker.union(numConvert, newNum);
+              //  }
             }
             if (isOpen(row + 1, col)) {
                 newNum = rcto1D(row + 1, col);
                 weightedU.union(numConvert, newNum);
+                percolateTracker.union(numConvert, newNum);
+
+                // if (isFull(row, col)) {
+               //     percolateTracker.union(numConvert, newNum);
+               // }
             }
         }
-
 
     }
 
@@ -165,7 +271,9 @@ public class Percolation {
             throw new IndexOutOfBoundsException("Row or Col Index out of bounds!");
         }
         int num = rcto1D(row, col);
-        return weightedU.connected(num, topUnion);
+        return percolateTracker.connected(num, topUnion);
+
+
     }
 
     public int numberOfOpenSites() {        // number of open sites
@@ -174,15 +282,7 @@ public class Percolation {
 
     //fix percolates method!
     public boolean percolates() {// does the system percolate?
-        System.out.println(weightedU.find(topUnion));
-        System.out.println("Connected? :" + weightedU.connected(topUnion, bottomUnion));
-        //doesn't percolate : if system cannot find topUnion and bottomUnion, then find() returns itself.
-        //if ((weightedU.find(topUnion) == topUnion) && (weightedU.find(bottomUnion) == bottomUnion) && (!weightedU.connected(topUnion, bottomUnion))) {
-          //  return false;
-        //}
-        //return true;
-
-        if (weightedU.connected(topUnion, bottomUnion)) {
+        if (weightedU.connected(topUnion, bottomUnion) && (percolateTracker.find(topUnion) != topUnion)) {
             return true;
         } else {
             return false;
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,53 @@
-package hw2;                       
+package hw2;
+
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 
 public class PercolationStats {
+    private Percolation p;
+    private int row;
+    private int col;
+    private int percolationThreshold;
+    private int[] percThres;
 
+    public PercolationStats(int N, int T) {  // perform T independent experiments on an N-by-N grid
+        if ((N <= 0) || (T <= 0)) {
+            throw new IllegalArgumentException("ERROR: Incorrect arguments for N or T");
+        }
+        //p = new Percolation(N);
+        percThres = new int[T];
+
+        while (T >= 0) {
+            p = new Percolation(N);
+            //keep opening until percolates
+            while (!p.percolates()) { // check for nulls!!
+                row = (StdRandom.uniform(N)); // [0, N)
+                col = (StdRandom.uniform(N)); // [0, N)
+                p.open(row, col);
+            }
+
+            percolationThreshold = p.numberOfOpenSites() / (N * N);
+            percThres[T] = percolationThreshold;
+        }
+
+    }
+
+    public double mean() {                    // sample mean of percolation threshold
+        return StdStats.mean(percThres);
+
+    }
+
+    public double stddev() {                 // sample standard deviation of percolation threshold
+        return StdStats.stddev(percThres);
+
+    }
+
+    public double confidenceLow() {          // low  endpoint of 95% confidence interval
+        return mean() - ((1.96 * stddev()) / Math.sqrt(percThres.length));
+    }
+
+    public double confidenceHigh() {         // high endpoint of 95% confidence interval
+        return mean() + ((1.96 * stddev()) / Math.sqrt(percThres.length));
+    }
 }                       
