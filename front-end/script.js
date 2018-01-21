8e623db856b877e4676239d9b2f462c57bc4de45
katie-gu <katiegu555@gmail.com>
2018-01-20 23:36:31 -0800

    Working on home page.

--- front-end/script.js
+++ front-end/script.js
@@ -1,9 +1,13 @@
+/*
+uses JQuery
+*/
+
 function onSignIn(googleUser)
 {
 	var profile=googleUser.getBasicProfile();
-	$(".g-signin2").css("display", "none");
-	$(".data").css("display", "block");
-	$("#pic").attr("src", profile.getImageUrl());
+	$(".g-signin2").css("display","none");
+	$(".data").css("display","block");
+	$("#pic").attr("src",profile.getImageUrl());
 	$("#email").text(profile.getEmail());
 }
 
@@ -17,4 +21,26 @@ function signOut()
 		$(".g-signin2").css("display", "block");
 		$(".data").css("display", "none")
 	});
+}
+
+/* dropdown */
+function toggleDropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
