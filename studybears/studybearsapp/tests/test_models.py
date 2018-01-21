e07c6b98053449609d51054607984b19f833e9a9
yaju-m <yaju-m@users.noreply.github.com>
2018-01-20 20:15:46 -0800

    testing and things work!

--- studybears/studybearsapp/tests/test_models.py
+++ studybears/studybearsapp/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from studybearsapp.models import StudyGroups, Profile
 from django.contrib.auth.models import User 
+from django.contrib.auth import authenticate 
 
 # Create your tests here.
 class YourTestClass(TestCase):
@@ -28,6 +29,14 @@ class YourTestClass(TestCase):
         # self.assertTrue(study_group_1.size == 3)
 
     def test_authenticate(self):
+        katie = Profile.objects.get(name="Katie", email="katiegu@berkeley.edu", phone_number=1)
+        User.objects.create_user('katie', "katie.email", 'test_password')
+        tester= authenticate(username = 'katie', password= 'test_password')
+        if tester is not None: 
+            print("authenticated correctly!")
+        else: 
+            print("didn't authenticate :(")
+
         #Test that logging into katie's account works, and that logging in with a wrong username/pw does not. 
 
 
@@ -41,8 +50,11 @@ class YourTestClass(TestCase):
         Profile.objects.create(name="Katie", email="katiegu@berkeley.edu", phone_number=1, user=katie)
         vera = User.objects.create_user('v_username', "katie_email", 'katie_password')
         Profile.objects.create(name="Vera", email="veraw@berkeley.edu", phone_number=2, user=vera)
-        #Profile.objects.create(name="Jhinuk", email="jhinukb@berkeley.edu", phone_number=222333444)
-        #Profile.objects.create(name="Yaju", email="yajum@berkeley.edu", phone_number=555666777)
+        jhinuk= User.objects.create_user('j_username', "katie_email", 'katie_password')
+        Profile.objects.create(name="Jhinuk", email="jhinukb@berkeley.edu", phone_number=2, user=jhinuk)
+        yaju= User.objects.create_user('y_username', "katie_email", 'katie_password')
+        Profile.objects.create(name="Yaju", email="yajum@berkeley.edu", phone_number=5, user=yaju)
+
 
     def test_add_past_capacity(self):
         study_group_1 = StudyGroups.objects.get(course="CS 70", location="Moffitt", size=0, capacity=3)
@@ -50,15 +62,16 @@ class YourTestClass(TestCase):
         #, studyemail="katiegu@berkeley.edu", phone_number=8185194728)
         katie.add_new_member(study_group_1)
         self.assertTrue(study_group_1.size == 1)
-        vera = Profile.objects.get(name="Vera", email="veraw@berkeley.edu", phone_number=2)
+        vera = Profile.objects.get(name="Vera")
         vera.add_new_member(study_group_1)
-        #self.assertTrue(study_group_1.size == 2)
-        #jhinuk = User.objects.get(name="Jhinuk", email="jhinukb@berkeley.edu", phone_number=222333444)
-        # jhinuk.add_new_member(study_group_1)
-        # self.assertTrue(study_group_1.size == 3)
-        # yaju = User.objects.get(name="Yaju", email="yajum@berkeley.edu", phone_number=555666777)
-        # yaju.add_new_member(study_group_1)
-        # self.assertTrue(study_group_1.size == 3)
+        self.assertTrue(study_group_1.size == 2)
+        jhinuk = Profile.objects.get(name="Jhinuk")
+        jhinuk.add_new_member(study_group_1)
+        self.assertTrue(study_group_1.size == 3)
+        yaju = Profile.objects.get(name="Yaju")
+        yaju.add_new_member(study_group_1)
+        self.assertTrue(study_group_1.size == 3)
+        print("past capacity not allowed!")
 
     def tearDown(self):
        #Clean up run after every test method.
